0555de22e067be58bebc636c61a06c8ddd7817ac
2VIPER2 <86585260+2VIPER2@users.noreply.github.com>
2021-06-28 00:00:05 -0500

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae exercitationem reiciendis qui optio labore neque totam ullam distinctio nostrum iure. Id eaque, ad pariatur necessitatibus sint omnis consequuntur commodi dolores.</p>
+    </div>
+  );
+}
+
+export default Info;
